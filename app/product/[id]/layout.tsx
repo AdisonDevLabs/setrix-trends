d7837d769e109a0932add295c776dc608571367a
Adison-tech <adisoncheruiyot55@gmail.com>
2026-07-06 13:22:38 +0300

    update

--- app/product/[id]/layout.tsx
+++ app/product/[id]/layout.tsx
@@ -1,8 +1,6 @@
 // app/product/[id]/layout.tsx
 
 // This tells Cloudflare to execute this specific dynamic path on the Edge Network
-export const runtime = 'edge';
-
 export default function ProductLayout({
   children,
 }: {
